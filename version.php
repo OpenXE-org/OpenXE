a78571aa45c4ce74018b3c2a2b210991cb000147
Xenomporio <>
2022-09-07 17:16:04 +0200

    1.3.1

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
 
 $version="OSS"; 
-$version_revision="1.0";
+$version_revision="1.3.1";
 $githash = file_get_contents("../githash.txt");
 if (!empty($githash)) {
   $version_revision .= " (".substr($githash,0,8).")";
