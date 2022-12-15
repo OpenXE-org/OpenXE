c519d8030a006d2308e32621342842c228e72dc8
OpenXE <>
2022-12-15 11:49:22 +0000

    Version 1.7

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
 
 $version="OSS"; 
-$version_revision="1.6";
+$version_revision="1.7";
 $githash = file_get_contents("../githash.txt");
 if (!empty($githash)) {
   $version_revision .= " (".substr($githash,0,8).")";
