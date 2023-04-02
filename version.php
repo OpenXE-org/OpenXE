8bdedcb72f11c30c0775490e72b6da705d782211
OpenXE <>
2023-04-02 17:44:11 +0200

    Develop version 1.8.1

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
 
 $version="OSS"; 
-$version_revision="1.8";
+$version_revision="D.1.8.1";
 $githash = file_get_contents("../githash.txt");
 if (!empty($githash)) {
   $version_revision .= " (".substr($githash,0,8).")";
