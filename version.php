9db337ce95f454dc9c3cd11ec8f49f1c90431b69
OpenXE <>
2023-04-24 16:23:21 +0200

    release 1.9

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
 
 $version="OSS"; 
-$version_revision="P.1.9";
+$version_revision="1.9";
 $githash = file_get_contents("../githash.txt");
 if (!empty($githash)) {
   $version_revision .= " (".substr($githash,0,8).")";
