1ab2bea2f1ef39008208508a79b2c3fbb45521b3
Andreas Palm <ap@ewsp.de>
2024-04-02 23:36:04 +0200

    Fix broken ShopUpdates after move to "versandpakete"

---
+++ classes/Modules/Onlineshop/Data/Shipment.php
@@ -0,0 +1,30 @@
+<?php
+
+// SPDX-FileCopyrightText: 2024 Andreas Palm
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+namespace Xentral\Modules\Onlineshop\Data;
+
+class Shipment
+{
+    /**
+     * @var int ID of the shipment (ERP domain)
+     */
+    public int $id;
+
+    /**
+     * @var string plain tracking number
+     */
+    public string $trackingNumber;
+
+    /**
+     * @var string URL to view tracking details
+     */
+    public string $trackingUrl;
+
+    /**
+     * @var string shipping method (after mapping to Shop domain)
+     */
+    public string $shippingMethod;
+}
