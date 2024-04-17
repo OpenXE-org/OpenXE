44b8dc0fe047950edd5ab85e725259c174e74f8e
Andreas Palm <ap@ewsp.de>
2024-04-17 16:42:57 +0200

    Remove Google Cloud Print

--- classes/Modules/GoogleApi/GoogleScope.php
+++ classes/Modules/GoogleApi/GoogleScope.php
@@ -6,9 +6,6 @@ namespace Xentral\Modules\GoogleApi;
 
 final class GoogleScope
 {
-    /** @var string CLOUDPRINT */
-    public const CLOUDPRINT = 'https://www.googleapis.com/auth/cloudprint';
-
     /** @var string CALENDAR */
     public const CALENDAR = 'https://www.googleapis.com/auth/calendar';
 
