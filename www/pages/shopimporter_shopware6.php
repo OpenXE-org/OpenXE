cd2a1631746e4957cc4564a0f4283d2f88463c61
Andreas Palm <ap@ewsp.de>
2024-07-05 21:34:01 +0200

    Fix shopware importer for shopware 6.6

--- www/pages/shopimporter_shopware6.php
+++ www/pages/shopimporter_shopware6.php
@@ -1430,14 +1430,16 @@ class Shopimporter_Shopware6 extends ShopimporterBase
                 continue;
             }
 
-            $mediaData = $this->shopwareRequest('POST', 'media?_response=true', []);
+            $mediaData = $this->shopwareRequest('POST', 'media?_response=true', [
+                'title' => $imageTitle,
+                'alt' => $imageAltText
+            ]);
             if(empty($mediaData['data']['id'])){
               $this->Shopware6Log('Error when creating media for sku: ' . $internalArticleData['nummer'],
                 ['mediaData' => $mediaData, 'title' => $imageTitle, 'text' => $imageAltText]);
               continue;
             }
             $mediaId = $mediaData['data']['id'];
-            $this->setMediaTitleAndAltText($mediaId, $imageTitle, $imageAltText);
 
             $mediaAssociationData = [
                 [
