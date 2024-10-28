a51136e3acef9bc0f59371771e8a94580271967a
OpenXE <>
2024-10-28 14:14:29 +0100

    bugfix mahnwesen berechnen

--- www/lib/class.erpapi.php
+++ www/lib/class.erpapi.php
@@ -34459,7 +34459,7 @@ function Firmendaten($field,$projekt="")
         * Recalculate payment status and skonto
         * using module rechnung
         */
-        public function rechnung_zahlstatus_berechnen($id) {
+        public function rechnung_zahlstatus_berechnen($id = null) {
             $rechnung = $this->app->loadModule('rechnung', false);
             if($rechnung !== null && method_exists($rechnung, 'rechnung_zahlstatus_berechnen')) {
               return $rechnung->rechnung_zahlstatus_berechnen($id);
