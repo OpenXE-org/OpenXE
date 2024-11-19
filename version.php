561b8c428ebb788be0d524306ea6ea69abbfcc5f
OpenXE <>
2024-11-19 16:44:03 +0100

    bugfix version.php display

--- version.php
+++ version.php
@@ -5,8 +5,8 @@ $version_revision="1.12";
 $gitinfo = file_get_contents("../gitinfo.json");
 if (!empty($gitinfo)) {
   $gitinfo = json_decode($gitinfo);
-  
-  if ($gitinfo->branch != 'master') {
+
+  if ($gitinfo->branch != 'master' && !empty($gitinfo->branch)) {
     $version_revision .= " (".substr($gitinfo->hash,0,8)." - ".$gitinfo->branch.")";
   }
   else {
