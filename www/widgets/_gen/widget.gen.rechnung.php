452b16b3573533c097b790d6baa9680152907d0d
OpenXE <>
2024-12-02 21:38:51 +0100

    erechnung checkbox

--- www/widgets/_gen/widget.gen.rechnung.php
+++ www/widgets/_gen/widget.gen.rechnung.php
@@ -112,6 +112,9 @@ class WidgetGenrechnung
     $field = new HTMLCheckbox("abweichendebezeichnung","","","1","0","0");
     $this->form->NewField($field);
 
+    $field = new HTMLCheckbox("erechnung","","","1","0","0");
+    $this->form->NewField($field);
+
     $field = new HTMLCheckbox("mahnwesenfestsetzen","","","1","0","0");
     $this->form->NewField($field);
 
