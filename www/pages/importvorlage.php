07503b020d4f40ca189b2f263a3a16a3afc1238b
OpenXE <>
2024-12-02 18:04:41 +0100

    bugfix importvorlage kontoauszuege

--- www/pages/importvorlage.php
+++ www/pages/importvorlage.php
@@ -5152,6 +5152,11 @@ class Importvorlage extends GenImportvorlage {
 
                 $row['buchung'] = $this->app->erp->ReplaceDatum(true,$row['buchung'],false);
 
+                // Sanitize input
+                foreach ($row as $key => $value) {
+                    $row[$key] = $this->app->DB->real_escape_string($value);
+                }
+
                 // Calculate hash
                 $hash_fields = array('buchung','soll','waehrung','buchungstext');
                 $hash_text = "";
