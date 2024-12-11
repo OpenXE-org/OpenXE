79b0313be5341092d0c59e8581018b912ec0ac24
OpenXE <>
2024-12-11 12:18:22 +0100

    bugfix XML-Rechnung Smarty

--- www/pages/rechnung.php
+++ www/pages/rechnung.php
@@ -1378,7 +1378,7 @@ class Rechnung extends GenRechnung
                 $smarty = new Smarty;
                 $directory = $this->app->erp->GetTMP().'/smarty/templates';
                 $smarty->setCompileDir($directory);
-                $smarty->assign('rechnung', $output);
+                $smarty->assign('rechnung', $result);
                 $output = $smarty->fetch('string:'.$template);
             }
         }
