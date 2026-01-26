8a8269049e48ff8320d3a795d6cbb4d66f5d6ac3
OpenXE <>
2026-01-26 10:37:10 +0100

    DpdDepot added field length truncation

--- classes/Carrier/DpdDepot/Data/AddressWithType.php
+++ classes/Carrier/DpdDepot/Data/AddressWithType.php
@@ -22,6 +22,33 @@ class Address
     public ?string $fax = null;
     public ?string $email = null;
     public ?string $comment = null;
+    
+    private $textLengths = [
+        'name1' => 35,
+        'name2' => 35,
+        'street' => 35,
+        'houseNo' => 8,
+        'state' => 2,
+        'country' => 2,
+        'zipCode' => 9,
+        'city' => 35,
+        'customerNumer' => 17,
+        'contact' => 35,
+        'phone' => 30,
+        'mobile' => 30,
+        'fax' => 30,
+        'email' => 100,
+        'comment' => 70,
+        'iaccount' => 50
+    ];
+    
+    public function limitTextLengths() {
+        foreach ($this->textLengths as $key => $value) {
+            if (isset($this->$key) && gettype($this->key == 'string')) {
+                $this->$key = substr($this->$key, 0, $value);
+            }
+        }
+    }
 }
 
 class AddressWithBusinessUnit extends Address
