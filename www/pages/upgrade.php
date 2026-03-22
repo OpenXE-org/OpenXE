fa1d2222bb385b476e92633656cf129bd3af89ee
OpenXE <>
2026-03-22 17:50:13 +0100

    upgrade branch upgrade GUI

--- www/pages/upgrade.php
+++ www/pages/upgrade.php
@@ -14,21 +14,22 @@ class upgrade {
             return;
 
         $this->app->ActionHandlerInit($this);
-        $this->app->ActionHandler("list", "upgrade_overview");        
+        $this->app->ActionHandler("list", "upgrade_overview");
         $this->app->DefaultActionHandler("list");
         $this->app->ActionHandlerListen($app);
     }
 
     public function Install() {
         /* Fill out manually later */
-    }  
- 
-    function upgrade_overview() {  
-    
+    }
+
+    function upgrade_overview() {
+
         $submit = $this->app->Secure->GetPOST('submit');
         $verbose = $this->app->Secure->GetPOST('details_anzeigen') === '1';
         $db_verbose = $this->app->Secure->GetPOST('db_details_anzeigen') === '1';
         $force = $this->app->Secure->GetPOST('erzwingen') === '1';
+        $branch = $this->app->Secure->GetPOST('branch');
 
       	$this->app->Tpl->Set('DETAILS_ANZEIGEN', $verbose?"checked":"");
       	$this->app->Tpl->Set('DB_DETAILS_ANZEIGEN', $db_verbose?"checked":"");
@@ -42,6 +43,37 @@ class upgrade {
         $this->app->Tpl->Set('UPGRADE_DB_VISIBLE', "hidden");
 
         $directory = dirname(getcwd())."/upgrade";
+        $remote_file_name = $directory."/data/remote.json";
+
+        $remote_info_contents = file_get_contents($remote_file_name);
+        if (!$remote_info_contents) {
+            $this->app->Tpl->AddMessage('error',"Unable to load $remote_file_name");
+        }
+        $remotes = json_decode($remote_info_contents, true);
+
+        $branches = array_column($remotes, 'name');
+
+        $table = new EasyTable($this->app);
+        $table->headings = array('','Name','Status','Beschreibung');
+
+        foreach ($remotes as $remote) {
+            $checked = '';
+            $status = '';
+            $disabled = '';
+            if ($remote['active']) {
+                $current_branch = $remote['name'];
+                $status = 'aktiv';
+                $checked = ' checked';
+            }
+            if (!$remote['enabled']) {
+                $status = 'gesperrt';
+                $disabled = ' disabled';
+            }
+            $select = '<input type="radio" name="branch" value="'.$remote['name'].'"'.$checked.$disabled.'>';
+            $row = array($select,$remote['name'],$status,$remote['description']['de']);
+            $table->AddRow($row);
+        }
+        $table->DisplayNew('BRANCHES','Status','noAction');
 
         switch ($submit) {
             case 'check_upgrade':
@@ -58,10 +90,16 @@ class upgrade {
                                 force: $force,
                                 connection: false,
                                 origin: false,
-                                drop_keys: false
+                                drop_keys: false,
+                                do_migrate: false
                 );
             break;
             case 'do_upgrade':
+
+                if ($branch != $remote['name']) {
+                    $do_migrate = true;
+                }
+
                 unlink($logfile);
                 upgrade_main(   directory: $directory,
                                 verbose: $verbose,
@@ -74,9 +112,11 @@ class upgrade {
                                 force: $force,
                                 connection: false,
                                 origin: false,
-                                drop_keys: false
+                                drop_keys: false,
+                                do_migrate: $do_migrate,
+                                upgrade_branch: $branch
                 );
-            break;    
+            break;
             case 'check_db':
                 $this->app->Tpl->Set('UPGRADE_DB_VISIBLE', "");
                 unlink($logfile);
@@ -91,9 +131,10 @@ class upgrade {
                                 force: $force,
                                 connection: false,
                                 origin: false,
-                                drop_keys: false
+                                drop_keys: false,
+                                do_migrate: false
                 );
-            break;    
+            break;
             case 'do_db_upgrade':
                 $this->app->Tpl->Set('UPGRADE_DB_VISIBLE', "");
                 unlink($logfile);
@@ -108,20 +149,20 @@ class upgrade {
                                 force: $force,
                                 connection: false,
                                 origin: false,
-                                drop_keys: false
+                                drop_keys: false,
+                                do_migrate: false
                 );
-            break;    
+            break;
             case 'refresh':
             break;
         }
 
         // Read results
-        $result = file_get_contents($logfile);             
+        $result = file_get_contents($logfile);
+        $this->app->Tpl->Set('CURRENTBRANCH', $current_branch);
         $this->app->Tpl->Set('CURRENT', $this->app->erp->Revision());
         $this->app->Tpl->Set('OUTPUT_FROM_CLI',nl2br($result));
         $this->app->Tpl->Parse('PAGE', "upgrade.tpl");
-    }   
-    
-
+    }
 }
 
